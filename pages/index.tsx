b932493deedbd8d912feccfdbd964c06c7bdd2cc
Monjur Khuda <kiatbrooklyn@gmail.com>
2022-03-23 16:19:27 -0400

    login page redesign

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import { Button, Flex, Link } from '@chakra-ui/react'
+import { Button, Flex, Link, Heading } from '@chakra-ui/react'
 import Head from 'next/head'
 
 export default function Home() {
   return (
     <Flex
-      height={[null, null, '100vh']}
+      height="100vh"
       width="100%"
-      flexDir={['column', 'column', 'row']}
+      flexDir="column"
       overflow="hidden"
       justifyContent="center"
       alignItems="center"
@@ -14,6 +14,28 @@ export default function Home() {
       <Head>
         <title>Overview. M: Project Management Simplified</title>
       </Head>
+
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        alignContent="center"
+        backgroundColor="black"
+        w="50px"
+        h="50px"
+        mb={[2, 2, 2]}
+      >
+        <Heading fontSize="2xl" color="white">
+          M
+        </Heading>
+        <Heading fontSize="4xl" color="yellow.400">
+          .
+        </Heading>
+      </Flex>
+
+      <Heading size="md" mb={10}>
+        Project Management Simplified
+      </Heading>
+
       <Link href="/overview">
         <Button size="lg" colorScheme="purple">
           Login
