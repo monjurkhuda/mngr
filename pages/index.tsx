8dd86733848c07a94a21f0ca8590d0d540affef5
Monjur Khuda <kiatbrooklyn@gmail.com>
2022-03-24 18:32:53 -0400

    demo account details

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Link, Heading } from '@chakra-ui/react'
+import { Button, Flex, Link, Heading, Text } from '@chakra-ui/react'
 import Head from 'next/head'
 
 export default function Home() {
@@ -32,15 +32,34 @@ export default function Home() {
         </Heading>
       </Flex>
 
-      <Heading size="md" mb={10}>
+      <Heading size="md" mb={4}>
         Project Management Simplified
       </Heading>
 
       <Link href="/overview">
-        <Button size="lg" colorScheme="purple">
+        <Button size="lg" colorScheme="purple" boxShadow="base">
           Login
         </Button>
       </Link>
+
+      <Flex
+        direction="column"
+        p={4}
+        borderRadius={10}
+        backgroundColor="gray.50"
+        mt={4}
+        boxShadow="base"
+      >
+        <Text size="md" fontWeight="600" mb={1}>
+          Demo Account
+        </Text>
+
+        <Text size="md" mb={1}>
+          Username: demo@demo.com
+        </Text>
+
+        <Text size="md">Password: @Demouser1234</Text>
+      </Flex>
     </Flex>
   )
 }
